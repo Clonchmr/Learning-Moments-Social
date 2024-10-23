bb5d0406c2ac3ed4e8754137e2d2984e14ccdf46
Matthew Clonch <matthewrclonch@gmail.com>
2024-10-23 14:44:46 -0500

    added filter functionality to search bar and topic dropdown

--- src/components/AllBlogs.jsx
+++ src/components/AllBlogs.jsx
@@ -1,46 +1,83 @@
-import { useEffect } from "react"
-import { useState } from "react"
-import { getAllBlogs } from "../services/allBlogs"
-import { BlogPost } from "./BlogPost"
-import "./blogs.css"
-import { NavBar } from "./NavBar"
+import { useEffect } from "react";
+import { useState } from "react";
+import { getAllBlogs } from "../services/allBlogs";
+import { BlogPost } from "./BlogPost";
+import "./blogs.css";
+import { NavBar } from "./NavBar";
 
-export const AllBlogs = () => { 
-    const [allBlogs, setAllBlogs] = useState([])
+export const AllBlogs = () => {
+  const [allBlogs, setAllBlogs] = useState([]);
+  const [filteredBlogs, setFilteredBlogs] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [selectedTopic, setSelectedTopic] = useState("");
 
-    useEffect(() => {
-        getAllBlogs().then((blogsArray) => {
-            setAllBlogs(blogsArray)
-        })
-    }, [])
+  useEffect(() => {
+    getAllBlogs().then((blogsArray) => {
+      setAllBlogs(blogsArray);
+      setFilteredBlogs(blogsArray);
+    });
+  }, []);
 
-    return (
-        <div className="all-posts-container">
-            <div>
-               <NavBar />
-            </div>
-            <div className="header-choices">
-                <div className="search-bar">
-                    <input className="header_choices" type="text" placeholder="Search For Post..."></input>
-                </div>
-                <div className="topic-choice">
-                    <select name="topics" className="header_choices">
-                        <option value="">Choose Topic</option>
-                        {[...new Set(allBlogs.map(blog => blog.topic.name))].map(topicName =>  {
-                            return (<option value={topicName}>{topicName}</option>)
-                    })}
-                    </select>
-                </div>
-            </div>
-            <div className="posts-container">
-                {allBlogs.map((blog) => {
-                    return(
-                    <BlogPost blog={blog}/>
-                    )
-                })
-                
-                }
-            </div>
+  useEffect(() => {
+    let foundBlog = allBlogs;
+
+    if (selectedTopic !== "") {
+      foundBlog = foundBlog.filter((blog) => blog.topic.name === selectedTopic);
+    }
+
+    if (searchText !== "") {
+      foundBlog = foundBlog.filter((blog) =>
+        blog.title.toLowerCase().includes(searchText.toLowerCase())
+      );
+    }
+
+    setFilteredBlogs(foundBlog);
+  }, [searchText, selectedTopic, allBlogs]);
+
+  const handleDropdown = (event) => {
+    setSelectedTopic(event.target.value);
+  };
+
+  return (
+    <div className="all-posts-container">
+      <div>
+        <NavBar />
+      </div>
+      <div className="header-choices">
+        <div className="search-bar">
+          <input
+            className="header_choices"
+            type="text"
+            placeholder="Search For Post..."
+            onChange={(event) => {
+              setSearchText(event.target.value);
+            }}
+          ></input>
         </div>
-    )
-}
+        <div className="topic-choice">
+          <select
+            name="topics"
+            className="header_choices"
+            onChange={handleDropdown}
+          >
+            <option value="">Choose Topic</option>
+            {[...new Set(allBlogs.map((blog) => blog.topic.name))].map(
+              (topicName) => {
+                return (
+                  <option key={topicName} value={topicName}>
+                    {topicName}
+                  </option>
+                );
+              }
+            )}
+          </select>
+        </div>
+      </div>
+      <div className="posts-container">
+        {filteredBlogs.map((blog) => {
+          return <BlogPost blog={blog} key={blog.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,20 +1,14 @@
 export const BlogPost = ({ blog }) => {
-    return (
-        <div className="post">
-            <h3 className="post-title interior-borders">
-                {blog.title}
-            </h3>
-            <div className="topic-likes">
-                <div className="blog-topic  interior-borders">
-                    {blog.topic.name}
-                </div>
-                <div className="blog-likes interior-borders">
-                    {blog.likes.length} {blog.likes.length === 1 ? "Like": "Likes"}
-                </div>
-            </div>
-            <div className="blog-body  interior-borders">
-                {blog.body}
-            </div>
+  return (
+    <div className="post">
+      <h3 className="post-title interior-borders">{blog.title}</h3>
+      <div className="topic-likes">
+        <div className="blog-topic  interior-borders">{blog.topic.name}</div>
+        <div className="blog-likes interior-borders">
+          {blog.likes.length} {blog.likes.length === 1 ? "Like" : "Likes"}
         </div>
-    )
-}
+      </div>
+      <div className="blog-body  interior-borders">{blog.body}</div>
+    </div>
+  );
+};
